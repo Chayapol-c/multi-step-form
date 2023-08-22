0d2a8db047c96656fd54f89bd4875d37c8de2cc5
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2023-08-22 22:17:27 +0700

    sidebar background image

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,19 +1,38 @@
 import { StepLabel } from "./StepLabel";
-import bgSidebarDesktop from "../assets/images/bg-sidebar-desktop.svg"
+import bgSidebarDesktop from "../assets/images/bg-sidebar-desktop.svg";
+import bgSidebarMobile from "../assets/images/bg-sidebar-mobile.svg";
 import { currentStepAtom } from "../atom";
 import { useAtom } from "jotai";
 
 function Sidebar() {
-  const [currentStep] = useAtom(currentStepAtom)
+  const [currentStep] = useAtom(currentStepAtom);
+
   return (
     <div
-      className={`p-6 bg-sidebar-desktop min-w-[274px] min-h-[568px]`}
-      style={{backgroundImage: `url(${bgSidebarDesktop})`}}
+      className={`p-6 md:min-w-[274px] md:min-h-[568px] md:block flex justify-center items-center gap-x-6 relative`}
     >
-      <StepLabel stepNumber="1" label="your info" isCurrentStep={currentStep === 1}/>
-      <StepLabel stepNumber="2" label="select plan" isCurrentStep={currentStep === 2}/>
-      <StepLabel stepNumber="3" label="add-ons" isCurrentStep={currentStep === 3}/>
-      <StepLabel stepNumber="4" label="summary" isCurrentStep={currentStep >= 4}/>
+      <img className="absolute top-0 left-0 w-content h-content w-full md:hidden -z-10" src={bgSidebarMobile} alt="mobile sidebar background"></img>
+      <img className="hidden md:block absolute top-0 left-0 -z-10 w-content h-content bg-cover" src={bgSidebarDesktop} alt="desktop sidebar background" />
+      <StepLabel
+        stepNumber="1"
+        label="your info"
+        isCurrentStep={currentStep === 1}
+      />
+      <StepLabel
+        stepNumber="2"
+        label="select plan"
+        isCurrentStep={currentStep === 2}
+      />
+      <StepLabel
+        stepNumber="3"
+        label="add-ons"
+        isCurrentStep={currentStep === 3}
+      />
+      <StepLabel
+        stepNumber="4"
+        label="summary"
+        isCurrentStep={currentStep >= 4}
+      />
     </div>
   );
 }
