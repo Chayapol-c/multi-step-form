3a42eff26b3b688e9b1dfd5c89289fa64a280382
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2023-08-22 23:32:52 +0700

    mobile finish step

--- src/components/FinishStep.tsx
+++ src/components/FinishStep.tsx
@@ -3,8 +3,8 @@ import thankyouIcon from "../assets/images/icon-thank-you.svg";
 function FinishStep() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center min-w-[660px]">
-        <img src={thankyouIcon} alt="thankyou icon" className="mb-8" />
+      <div className="md:py-8 md:px-24 px-8 py-16 md:min-w-[660px] bg-white rounded-md md:m-6 mx-6 mb-32 text-center">
+        <img src={thankyouIcon} alt="thankyou icon" className="mb-8 mx-auto" />
 
         <p className="text-marine-blue text-3xl font-bold mb-4">Thank you!</p>
         <p className="text-cool-gray max-w-md mx-auto text-center">
